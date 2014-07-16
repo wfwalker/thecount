7468d0f6850802689a38a9c0f2ec04990dde3417
Bill Walker <bwalker@mozilla.com>
2014-07-15 19:50:33 -0700

    better version of sortable applisting table

--- scripts/installHelpers.js
+++ scripts/installHelpers.js
@@ -138,5 +138,20 @@ $(document).ready(function() {
 
 	// set up table sorting behavior
 
-	$('.tablesorter-table').tablesorter();
+	$('.tablesorter-table').tablesorter({
+	    theme : 'blue',
+	 
+	    headers: { 0: { sorter: false }, 1: { sorter: true }, 2: { sorter: false } },
+	 
+	    // header layout template; {icon} needed for some themes
+	    headerTemplate : '{content}{icon}',
+	 
+	    // initialize column styling of the table
+	    widgets : ["columns"],
+	    widgetOptions : {
+			// change the default column class names
+			// primary is the first column sorted, secondary is the second, etc
+			columns : [ "primary", "secondary", "tertiary" ]
+	    }
+	});
 });
